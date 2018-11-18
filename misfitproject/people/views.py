8d17729cab9849c364dfcfe91437acd357b751f2
cjahangir <jahangir.cse09@gmail.com>
2018-11-18 16:12:10 +0600

    added profile picture

--- misfitproject/people/views.py
+++ misfitproject/people/views.py
@@ -4,23 +4,25 @@ from django.contrib.auth import get_user_model
 from account.views import SignupView
 from account import signals
 from account.forms import SignupForm, LoginUsernameForm
+from forms import UserSignupForm
 # Create your views here.
 
 
 class UserSignup(SignupView):
+    form_class = UserSignupForm
 
     def after_signup(self, form):
-        signals.user_signed_up.send(sender=SignupForm, user=self.created_user, form=form)
+        signals.user_signed_up.send(sender=UserSignupForm, user=self.created_user, form=form)
 
         #added the created user to the desired group
         user = self.created_user
         user_type = self.request.POST['usertype']
         if user_type == 'MANAGER':
-            user_group = Group.objects.get(name='manager')
+            user_group = Group.objects.get_or_create(name='manager')
             user_group.user_set.add(user)
         elif user_type == 'HR':
-            user_group = Group.objects.get(name='hr')
+            user_group = Group.objects.get_or_create(name='hr')
             user_group.user_set.add(user)
         else:
-            user_group = Group.objects.get(name='member')
+            user_group = Group.objects.get_or_create(name='member')
             user_group.user_set.add(user)
